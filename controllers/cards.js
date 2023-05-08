1baa199f049cad713dbd4e5ac30f7bda073ab24b
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-08 18:25:46 +0300

    feat: test zaprosov

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,9 +13,13 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card
     .create({ name, link, owner })
-    .then((card) => res.status(201).send(card))
+    .then((card) => {
+      res.status(201).send(card);
+    })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (
+        res.status(err.name === 'CastError' || err.name === 'ValidationError')
+      ) {
         return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
       }
       return res.status(500).send({ message: 'Произошла ошибка' });
@@ -25,51 +29,74 @@ module.exports.createCard = (req, res) => {
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .orFail()
-    .then((card) => res.status(200).send(card))
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Карточка не найдена' });
+      } else {
+        res.send(card);
       }
+    })
+    .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(400).send({ message: 'Переданы некорректные данные при удалении карточки' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
       }
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
 module.exports.likeCard = (req, res) => {
-  Card
-    .findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    )
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
     .orFail()
-    .then((card) => res.status(200).send(card))
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Карточка не найдена' });
+      } else {
+        res.send(card);
       }
+    })
+    .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
       }
       return res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
-  .orFail()
-  .then((card) => res.status(200).send(card))
-  .catch((err) => {
-    if (err.name === 'DocumentNotFoundError') {
-      return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
-    }
-    if (err.name === 'CastError') {
-      return res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' });
-    }
-    return res.status(500).send({ message: 'Произошла ошибка' });
-  });
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(() => {
+      const newError = new Error();
+      newError.name = 'DocumentNotFoundError';
+      throw newError;
+    })
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Карточка не найдена' });
+      } else {
+        res.send(card);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
