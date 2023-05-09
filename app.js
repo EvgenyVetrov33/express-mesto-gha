0f105b01ddc9a615daeb9d1e62132920e3fa108d
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-09 20:50:28 +0300

    feat: try actions

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ mongoose.connect('mongodb://127.0.0.1/mestodb ', {
   useNewUrlParser: true,
 });
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '6457b0eaf5d12c76611540ec',
-//   };
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6457b0eaf5d12c76611540ec',
+  };
 
-//   next();
-// });
+  next();
+});
 
 app.use('/', router);
 // app.use((req, res) => {
